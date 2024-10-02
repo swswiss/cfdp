e2fc469eabd5a532888a46b4805d7992c7b764d9
swswiss <stefanachesilviu1993@gmail.com>
2024-10-02 17:43:51 +0300

    added notifications

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,10 @@
 class DashboardController < ApplicationController
-    before_action :authenticate_user!
-    
-    def index
-    end
+	before_action :authenticate_user!
+	
+	def index
+		flash.now[:notice] ={
+			title: "Successfully Sign In", 
+			body: "Welcome to our application"
+		}
+	end
 end
