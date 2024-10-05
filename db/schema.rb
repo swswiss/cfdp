f9d2c30043646b8256f92d7960845e40935fe852
swswiss <stefanachesilviu1993@gmail.com>
2024-10-06 02:42:01 +0300

    added_profile_settings_styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_04_030026) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_05_212302) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +44,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_10_04_030026) do
     t.string "firstname"
     t.string "lastname"
     t.string "username"
+    t.string "role", default: "student"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
