503dd42b45a9921e8c52b2c57dfe8adc85caeb41
swswiss <stefanachesilviu1993@gmail.com>
2024-11-09 19:46:02 +0200

    hell

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -8,12 +8,6 @@
 #
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
 
-WickedPdf.configure do |config|
-  if Rails.env.production?
-    # Path to where the actual gem binary is extracted:
-    #config.exe_path = "#{ENV['GEM_HOME']}/gems/wkhtmltopdf-binary-#{Gem.loaded_specs['wkhtmltopdf-binary'].version}/bin/wkhtmltopdf_macos_cocoa"
-
-    # Or a copied binary in the /bin/ path:
-    config.exe_path = Rails.root.join('bin/wkhtmltopdf').to_s
-  end
-end
+WickedPdf.config = {
+  exe_path: "#{ENV['GEM_HOME']}/gems/wkhtmltopdf-binary-#{Gem.loaded_specs['wkhtmltopdf-binary'].version}/bin/wkhtmltopdf"
+}
