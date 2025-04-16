b502f808037259df2619ee5d90d6e131cf96049c
swswiss <stefanachesilviu1993@gmail.com>
2025-04-16 11:34:15 +0300

    extra

--- app/controllers/instance_bridges_controller.rb
+++ app/controllers/instance_bridges_controller.rb
@@ -206,7 +206,7 @@ class InstanceBridgesController < ApplicationController
         # Turbo Stream will append new avatars to the avatar list
         flash[:success] = "Photos uploaded successfully."
         render turbo_stream: [turbo_stream.append("avatars-list", 
-                                                 html: render_avatar(@instance_bridge.avatars.last)),
+                                                 html: render_avatar(@instance_bridge.avatars.last(params[:instance_bridge][:avatars].size-1))),
         turbo_stream.update( "flash", partial: "layouts/flash")]
       end
       format.html do
@@ -216,9 +216,8 @@ class InstanceBridgesController < ApplicationController
   end
 
   def render_avatar(avatar_url)
-    "<div id='avatars-list'>
-      <div class='mx-auto max-w-4xl px-4 sm:px-6 lg:max-w-7xl lg:px-8'>
-        <div class='grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8'>
+    content = avatar_url.map do |avatar_url|
+    "
           <div id='avatar-#{avatar_url.id}' class='group relative' data-controller='zoom'>
             <img src='#{rails_blob_path(avatar_url, only_path: true)}'
                  alt='Avatar Image'
@@ -228,16 +227,26 @@ class InstanceBridgesController < ApplicationController
               <form action='#{destroy_avatar_bridge_instance_bridge_path(@bridge, @instance_bridge, avatar_url: avatar_url)}'
                     method='POST' data-turbo-stream='true'>
                 <button type='submit' class='bg-indigo-500 text-white rounded-full p-1 hover:bg-red-700'>
-                  <svg class='w-2 h-2' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 20 20' stroke='currentColor'>
-                    <path stroke-linecap='round' stroke-linejoin='round' stroke-width='4' d='M6 6L14 14M6 14L14 6'/>
-                  </svg>
+                  <svg class='w-3 h-3' xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
+                  <path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0'/>
+                </svg>
+                  
                 </button>
               </form>
             </div>
-          </div>
+      
+    </div>"
+  end.join
+
+  "
+    <div id='avatars-list'>
+      <div class='mx-auto max-w-4xl px-4 sm:px-6 lg:max-w-7xl lg:px-8'>
+        <div class='grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8'>
+          #{content}
         </div>
       </div>
-    </div>".html_safe
+    </div>
+  ".html_safe
   end
   
 
